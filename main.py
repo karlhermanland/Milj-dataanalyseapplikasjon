28d7ce340bb9103b168d1479478a07d95316bb1c
alekslm <aleksalm@stud.ntnu.no>
2025-03-25 17:09:48 +0100

    fjernet clean-data relatert til yr

--- main.py
+++ main.py
@@ -16,17 +16,5 @@ def main():
     fetch_nasa_data()
     fetch_data_frost()
 
-    # Kjør datarensing og sammenslåing
-    merged_df, daily_avg = clean_and_merge_weather_data(
-        "data/raw/yr_extended_weather_data.csv",
-        "data/raw/nasa_extended_data.csv"
-    )
-
-    # Lagre renset og aggregert data
-    merged_df.to_csv("data/clean/cleaned_combined_weather_data.csv", index=False)
-    daily_avg.to_csv("data/clean/cleaned_daily_weather_summary.csv", index=False)
-
-    print("Ferdig! Data lagret i 'data/clean/'")
-
 if __name__ == "__main__":
     main()
